20ffae42611f3cc268019b9f7687640aecb02a18
charlesvtiu <charlesvtiu@gmail.com>
2017-10-03 21:26:29 +0800

    enhancements

--- javascript.js
+++ javascript.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 	showPictures('london');
 }) 
 
-var countryName = ['hongkong','boston','london','italy','greece','japan'];
-var countryCount = ['10','10','36','10','50','10'];
-var countrySeen = ['0','0','0','0','0','0'];
+var countryName = ['hongkong','boston','london','florence','venice','greece','okinawa'];
+var countryCount = ['10','10','36','10','10','10','10'];
+var countrySeen = ['0','0','0','0','0','0','0','0'];
 var lastCountry = "london";
 
 /* Set the width of the side navigation to 250px */
@@ -25,11 +25,12 @@ function showPictures(country) {
 	lastCountry = country;
 	var pos = countryName.indexOf(country);
 	var max = countryCount[pos];
-	console.log(max);
+	console.log(country + " "+ max);
 	console.log(countrySeen[pos]);
 	if (countrySeen[pos] == 0) { 
 		for (i = 1; i < max; i++ ) {
-			$('#'+country+'Container').append('<div class="col-md-4 col-sm-6 co-xs-12 gal-item"> <div class="box"> <a href="#" data-toggle="modal" data-target="#8"> <img src=pictures/photography/'+country+'/'+i+'.jpg> </a> <div class="modal fade" id="8" tabindex="-1" role="dialog"> <div class="modal-dialog" role="document"> <div class="modal-content"> <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button> <div class="modal-body"> <img src=pictures/photography/'+country+'/'+i+'.jpg> </div> <div class="col-md-12 description"> <h4>'+country+'</h4> </div> </div> </div> </div> </div> </div>');
+			var imgsrc='pictures/photography/'+country+'/'+i+'.jpg';
+			$('#'+country+'Container').append('<div class="col-md-4 col-sm-6 co-xs-12 gal-item"> <div class="box"> <a href="#" data-toggle="modal" data-target="#'+i+'"> <img src='+imgsrc+'> </a> <div class="modal fade" id="'+i+'" tabindex="-1" role="dialog"> <div class="modal-dialog" role="document"> <div class="modal-content"> <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button> <div class="modal-body"> <img src='+imgsrc+'> </div> <div class="col-md-12 description"> <h4>'+country+'</h4> </div> </div> </div> </div> </div> </div>');
 		}
 	}
 	$('#'+country+'Container').show();
